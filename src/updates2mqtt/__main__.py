eab5f627aa65cf9a84d55d66b183496efaaecf4f
Jey Burrows <jrb@jeymail.net>
2025-09-29 23:59:47 +0100

    remove run.py

--- src/updates2mqtt/__main__.py
+++ src/updates2mqtt/__main__.py
@@ -1,9 +1,14 @@
 """Detect component version change availability and broadcast on MQTT topic, suitable for HomeAssistant autodetect"""
 
-if __name__ == "__main__":
+
+def run() -> None:
     import asyncio
 
     from .app import App
 
     app = App()
     asyncio.run(app.run())
+
+
+if __name__ == "__main__":
+    run()
