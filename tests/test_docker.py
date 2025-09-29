b516a1264829116eb379a17c69b8ab7da143c111
Jey Burrows <jrb@jeymail.net>
2025-09-29 23:06:27 +0100

    ruff fix

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,6 +1,5 @@
 from unittest.mock import patch
 
-import pytest
 from docker import DockerClient  # type:ignore[import-not-found]
 from pytest_httpx import HTTPXMock
 from pytest_subprocess import FakeProcess  # type: ignore[import-not-found]
