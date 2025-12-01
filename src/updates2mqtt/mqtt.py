eab127139bcf6318deb83c434fd0992c7fc0c846
jey burrows <jrb@jeymail.net>
2025-12-01 06:00:47 +0000

    sub/unsub debug callbacks

--- src/updates2mqtt/mqtt.py
+++ src/updates2mqtt/mqtt.py
@@ -73,6 +73,8 @@ class MqttClient:
             self.client.on_connect = self.on_connect
             self.client.on_disconnect = self.on_disconnect
             self.client.on_message = self.on_message
+            self.client.on_subscribe = self.on_subscribe
+            self.client.on_unsubscribe = self.on_unsubscribe
 
             self.client.loop_start()
 
