b8c55bfc1257b7317ae0dc4480a8a8371294209b
Jey Burrows <jrb@jeymail.net>
2026-01-17 14:56:13 +0000

    common pkg

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -1,5 +1,7 @@
 import docker
 import httpx
+from pathlib import Path
+from updates2mqtt.config import Config, PackageUpdateInfo, load_package_info
 
 custom={}
 
@@ -7,9 +9,29 @@ def save_if_set(key: str, val: str | None) -> None:
     if val is not None:
         custom[key] = val
 
+def valurl(url)->bool:
+    try:
+       response=httpx.get(url)
+       return response.status_code
+    except:
+        pass
+    return False
+
 client=docker.from_env()
+common_pkgs=load_package_info(Path("common_packages.yaml"))
+
+def find_pkg(imgref):
+    for pkg in common_pkgs.values():
+         if pkg.docker.image_name == imgref:
+             return pkg
+    return None
+
 for c in client.containers.list():
-    
+
+    image_ref=c.attrs.get("Config", {}).get("Image") or c.image.tags[0]
+    image_name = image_ref.split(":")[0]
+    pkg=find_pkg(image_ref) or find_pkg(image_name)
+    print(pkg)
     image_version: str = c.labels.get("org.opencontainers.image.version")
     image_revision: str = c.labels.get("org.opencontainers.image.revision")
     source = c.labels.get("org.opencontainers.image.source")
@@ -18,16 +40,16 @@ for c in client.containers.list():
        save_if_set("diff_url", diff_url)
        release_url = f"{source}/releases/tag/{image_version}"
        save_if_set("release_url", release_url)
-       response=httpx.get(release_url)
-       if response.status_code==200:
+       if valurl(release_url):
            print("GOOD %s" % release_url)
        else:
            print("BROKEN %s" % release_url)
-       response=httpx.get(diff_url)
-       if response.status_code==200:
+       if valurl(diff_url):
            print("GOOD %s" % diff_url)
        else:
            print("BROKEN %s" % diff_url)
+    elif pkg:
+        release_url=pkg.release_notes_url.format({"image_version"
     else:
        print("No metadata for %s" % c.name)
 
