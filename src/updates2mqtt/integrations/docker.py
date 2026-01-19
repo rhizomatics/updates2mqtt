744bd475d9acf5d63a1bb8fd473d2b42e4565684
Jey Burrows <jrb@jeymail.net>
2026-01-19 00:22:31 +0000

    fetch

--- src/updates2mqtt/integrations/docker.py
+++ src/updates2mqtt/integrations/docker.py
@@ -389,8 +389,6 @@ class DockerProvider(ReleaseProvider):
             custom["repo_id"] = repo_id
             custom["git_repo_path"] = customization.git_repo_path
 
-            if registry_throttled:
-                custom["registry_throttled"] = True
             save_if_set("compose_path", c.labels.get("com.docker.compose.project.working_dir"))
             save_if_set("compose_version", c.labels.get("com.docker.compose.version"))
             save_if_set("compose_service", c.labels.get("com.docker.compose.service"))
@@ -399,25 +397,30 @@ class DockerProvider(ReleaseProvider):
             save_if_set("current_image_created", c.labels.get("opencontainers.image.created"))
             save_if_set("current_image_version", c.labels.get("opencontainers.image.version"))
             installed_version = c.labels.get("opencontainers.image.version")
+            # save_if_set("apt_pkgs", c_env.get("UPD2MQTT_APT_PKGS"))
 
-            if latest_digest is not None and latest_digest != NO_KNOWN_IMAGE and not registry_throttled:
-                # save_if_set("apt_pkgs", c_env.get("UPD2MQTT_APT_PKGS"))
-                os, arch = platform.split("/")[:2] if "/" in platform else (platform, "Unknown")
-                try:
-                    new_manifest = self.label_enricher.fetch_manifest(image_ref, os, arch, token=customization.registry_token)
-                except AuthError as e:
-                    logger.warning("Authentication error prevented Docker Registry entichment: %s", e)
-                    new_manifest = None
+            if registry_throttled:
+                custom["registry_throttled"] = True
+            else:
+                if latest_digest is not None and latest_digest != NO_KNOWN_IMAGE:
+                    os, arch = platform.split("/")[:2] if "/" in platform else (platform, "Unknown")
+                    try:
+                        new_manifest = self.label_enricher.fetch_manifest(
+                            image_ref, os, arch, token=customization.registry_token
+                        )
+                    except AuthError as e:
+                        logger.warning("Authentication error prevented Docker Registry entichment: %s", e)
+                        new_manifest = None
 
-                if new_manifest:
-                    annotations = new_manifest.get("annotations", {})
-                    save_if_set("latest_image_created", annotations.get("opencontainers.image.created"))
-                    save_if_set("source", annotations.get("org.opencontainers.image.source"))
-                    save_if_set("documentation_url", annotations.get("org.opencontainers.image.documentation"))
-                    save_if_set("description", annotations.get("org.opencontainers.image.description"))
-                    save_if_set("latest_image_version", annotations.get("opencontainers.image.version"))
-                    latest_version = annotations.get("org.opencontainers.image.version")
-                    custom.update(self.release_enricher.enrich(annotations, log) or {})
+                    if new_manifest:
+                        annotations = new_manifest.get("annotations", {})
+                        save_if_set("latest_image_created", annotations.get("opencontainers.image.created"))
+                        save_if_set("source", annotations.get("org.opencontainers.image.source"))
+                        save_if_set("documentation_url", annotations.get("org.opencontainers.image.documentation"))
+                        save_if_set("description", annotations.get("org.opencontainers.image.description"))
+                        save_if_set("latest_image_version", annotations.get("opencontainers.image.version"))
+                        latest_version = annotations.get("org.opencontainers.image.version")
+                        custom.update(self.release_enricher.enrich(annotations, log) or {})
 
             if custom.get("git_repo_path") and custom.get("compose_path"):
                 full_repo_path: Path = Path(cast("str", custom.get("compose_path"))).joinpath(
