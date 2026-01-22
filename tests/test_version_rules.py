e98e88b547ca8d770dc9023aa277287930800429
Jey Burrows <jrb@jeymail.net>
2026-01-22 14:57:34 +0000

    nicer shortcircuits

--- tests/test_version_rules.py
+++ tests/test_version_rules.py
@@ -5,19 +5,19 @@ from updates2mqtt.integrations.docker_enrich import DockerImageInfo
 
 def test_good_version_and_digest_matching() -> None:
     latest = installed = DockerImageInfo("foo", version="22.04", image_digest="b5c7fd5f595a")
-    assert select_versions(VersionPolicy.AUTO, installed, latest) == ("22.04", "22.04", "causualver-1-M")
-    assert select_versions(VersionPolicy.VERSION, installed, latest) == ("22.04", "22.04", "version-0-M")
-    assert select_versions(VersionPolicy.DIGEST, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-0-M")
+    assert select_versions(VersionPolicy.AUTO, installed, latest) == ("22.04", "22.04", "causualver-1-SDM")
+    assert select_versions(VersionPolicy.VERSION, installed, latest) == ("22.04", "22.04", "version-0-SDM")
+    assert select_versions(VersionPolicy.DIGEST, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-0-SDM")
     assert select_versions(VersionPolicy.VERSION_DIGEST, installed, latest) == (
         "22.04:b5c7fd5f595a",
         "22.04:b5c7fd5f595a",
-        "version-digest-0-M",
+        "version-digest-0-SDM",
     )
 
 
 def test_semver_matching() -> None:
     latest = installed = DockerImageInfo("foo", version="22.4.3", image_digest="b5c7fd5f595a")
-    assert select_versions(VersionPolicy.AUTO, installed, latest) == ("22.4.3", "22.4.3", "semver-1-M")
+    assert select_versions(VersionPolicy.AUTO, installed, latest) == ("22.4.3", "22.4.3", "semver-1-SDM")
 
 
 def test_good_version_and_digest_incremented() -> None:
@@ -36,10 +36,10 @@ def test_good_version_and_digest_incremented() -> None:
 def test_no_version_and_digest_matching() -> None:
     installed = DockerImageInfo("foo", image_digest="b5c7fd5f595a")
     latest = DockerImageInfo("foo", image_digest="b5c7fd5f595a")
-    assert select_versions(VersionPolicy.AUTO, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-4-M")
-    assert select_versions(VersionPolicy.VERSION, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-4-M")
-    assert select_versions(VersionPolicy.DIGEST, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-0-M")
-    assert select_versions(VersionPolicy.VERSION_DIGEST, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-4-M")
+    assert select_versions(VersionPolicy.AUTO, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-4-SDM")
+    assert select_versions(VersionPolicy.VERSION, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-4-SDM")
+    assert select_versions(VersionPolicy.DIGEST, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-0-SDM")
+    assert select_versions(VersionPolicy.VERSION_DIGEST, installed, latest) == ("b5c7fd5f595a", "b5c7fd5f595a", "digest-4-SDM")
 
 
 def test_git_local_versions() -> None:
